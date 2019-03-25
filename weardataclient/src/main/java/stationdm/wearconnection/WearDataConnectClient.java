52fe5847d6df84e762832b7008b94eb64fe0e19a
DanialZhang <zhang.qi@stationdm.com>
2019-03-25 16:19:46 +0800

    Upload the first version

---
+++ weardataclient/src/main/java/stationdm/wearconnection/WearDataConnectClient.java
@@ -0,0 +1,122 @@
+package stationdm.wearconnection;
+
+import android.content.Context;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.common.data.FreezableUtils;
+import com.google.android.gms.wearable.CapabilityClient;
+import com.google.android.gms.wearable.CapabilityInfo;
+import com.google.android.gms.wearable.DataClient;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.MessageClient;
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Daniel on 2019/3/25.
+ */
+public class WearDataConnectClient implements WearDataContract.Presenter, DataClient.OnDataChangedListener, MessageClient.OnMessageReceivedListener, CapabilityClient.OnCapabilityChangedListener {
+
+    private WearDataContract.Callback callback;
+
+    private Context context;
+
+    private String dataReceivePath;
+
+    private HashMap<String, String> keys = new HashMap<>();
+
+    public WearDataConnectClient(Context context) {
+        this.context = context;
+        keys.clear();
+    }
+
+    public void setCallback(WearDataContract.Callback callback) {
+        this.callback = callback;
+    }
+
+    public void setDataReceivePath(String dataReceivePath) {
+        this.dataReceivePath = dataReceivePath;
+    }
+
+    public void putKey(String key) {
+        keys.put(key, key);
+    }
+
+    @Override
+    public void send(String destinationPath, String key, String data) {
+        PutDataMapRequest putDataMapRequest = PutDataMapRequest.create(destinationPath);
+        putDataMapRequest.getDataMap().putString(key, data);
+        PutDataRequest request = putDataMapRequest.asPutDataRequest();
+        request.setUrgent();
+        Wearable.getDataClient(context.getApplicationContext()).putDataItem(request);
+    }
+
+    @Override
+    public void register() {
+        Wearable.getDataClient(context).addListener(this);
+        Wearable.getMessageClient(context).addListener(this);
+        Wearable.getCapabilityClient(context).addListener(this, Uri.parse("wear://"), CapabilityClient.FILTER_REACHABLE);
+    }
+
+    @Override
+    public void unregister() {
+        Wearable.getDataClient(context).removeListener(this);
+        Wearable.getMessageClient(context).removeListener(this);
+        Wearable.getCapabilityClient(context).removeListener(this);
+    }
+
+    @Override
+    public void onDataChanged(@NonNull DataEventBuffer dataEvents) {
+        final List<DataEvent> events = FreezableUtils
+                .freezeIterable(dataEvents);
+        for (DataEvent dataEvent : events) {
+            if (dataEvent.getType() == DataEvent.TYPE_CHANGED) {
+                Uri uri = dataEvent.getDataItem().getUri();
+                String path = uri.getPath();
+                if (path != null && path.equals(dataReceivePath)) {
+                    //inbound data chain,from watch to phone
+                    DataMapItem dataMapItem = DataMapItem
+                            .fromDataItem(dataEvent.getDataItem());
+
+                    for (String key : keys.values()) {
+                        String resultJson = dataMapItem.getDataMap().getString(key);
+                        if (callback != null) {
+                            callback.onStringDataReceived(key, resultJson);
+                        }
+                    }
+                }
+            }
+        }
+        dataEvents.release();
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull MessageEvent messageEvent) {
+
+    }
+
+    @Override
+    public void onCapabilityChanged(@NonNull CapabilityInfo capabilityInfo) {
+        if (capabilityInfo != null) {
+            boolean hasNodeNearby = false;
+            for (Node node : capabilityInfo.getNodes()) {
+                if (node.isNearby()) {
+                    hasNodeNearby = true;
+                    break;
+                }
+            }
+            if (callback != null) {
+                callback.onConnectionChanged(hasNodeNearby);
+            }
+        }
+    }
+}
---
+++ weardataclient/src/main/java/stationdm/wearconnection/WearDataContract.java
@@ -0,0 +1,29 @@
+package stationdm.wearconnection;
+
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.OnLifecycleEvent;
+
+/**
+ * Created by Daniel on 2019/3/25.
+ */
+public interface WearDataContract {
+
+    interface Presenter extends LifecycleObserver {
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        void register();
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+        void unregister();
+
+        void send(String destinationPath, String key, String data);
+    }
+
+    interface Callback {
+
+        void onConnectionChanged(boolean hasNodeNearby);
+
+        void onStringDataReceived(String key, String data);
+    }
+}
